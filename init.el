82840777b8e73b5037a98453ffa0101210a511c3
yamashita <t.yamashita1238@gmail.com>
2015-05-07 16:33:33 +0900

    for javascript

--- init.el
+++ init.el
@@ -188,3 +188,9 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; javascript
+(require 'js2-mode)
+(require 'tern)
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
