05ec16d0d4453353f7f0008f95fb6d61b3f884a6
yamashita <t.yamashita1238@gmail.com>
2015-05-07 16:32:54 +0900

    for octave

--- init.el
+++ init.el
@@ -172,3 +172,19 @@
 ;; flycheck
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; octave
+(add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tab-width 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
